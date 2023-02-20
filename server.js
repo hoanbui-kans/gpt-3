888faef427adf0e4195836e8c8bc7920b8747fce
Bùi Công Hoàn <117809011+hoanbui-kans@users.noreply.github.com>
2023-02-20 14:04:22 +0700

    update

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const app = require("express")();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const next = require("next");
-const secure = require('express-force-https');
 const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
@@ -13,12 +12,19 @@ io.on("connect", (socket) => {
   socket.broadcast.emit('a user connected');
 });
 
-if(!dev){
-  app.use(secure);
-}
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+      if (req.headers['x-forwarded-proto'] !== 'https')
+          // the statement for performing our redirection
+          return res.redirect('https://' + req.headers.host + req.url);
+      else
+          return next();
+  } else
+      return next();
+});
 
 nextApp.prepare().then(() => {
-  
+
   app.all("*", (req, res) => {
     return nextHandler(req, res);
   });
